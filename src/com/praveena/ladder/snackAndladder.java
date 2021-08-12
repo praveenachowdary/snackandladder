19c6a85c70b9a0a456ac4812304877b1ae384472
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-12 19:05:47 +0530

    snackandladder

--- src/com/praveena/ladder/snackAndladder.java
+++ src/com/praveena/ladder/snackAndladder.java
@@ -1,7 +1,9 @@
 package com.praveena.ladder;
 
 public class snackAndladder {
-    public static void main(String[] args) {
-        System.out.println("welcome to snackandladder");
+   
+        public int position=0;
+        public static void main(String[] args) {
+            System.out.println("welcome to snake and ladder problem");
+        }
     }
-}
